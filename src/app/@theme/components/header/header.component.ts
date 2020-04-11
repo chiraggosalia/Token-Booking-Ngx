8aa0f47628bdcb422f55d7caccff5a113b8ecd3e
Chirag Gosalia <chirag.gosalia@gmail.com>
2020-04-11 20:54:09 +0530

    minor change

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -50,7 +50,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.currentTheme = this.themeService.currentTheme;
 
-    this.user = 'Chirag'
+    this.user = 'Chirag';
 
     const { xl } = this.breakpointService.getBreakpointsMap();
     this.themeService.onMediaQueryChange()
